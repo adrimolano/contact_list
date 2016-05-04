66c78f5515089878e1dc5c25507c5badb4a8766c
Adriana Molano <molanoa@delmar.ca>
2016-05-04 19:15:01 -0400

    Added files via upload

---
+++ contact.rb
@@ -0,0 +1,68 @@
+require 'csv'
+contact = CSV.read('contact.csv')
+
+# Represents a person in an address book.
+# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
+class Contact
+
+  attr_accessor :name, :email, :id
+
+
+    def initialize (name, email, id)
+      @name = name
+      @email = email 
+      @id = id 
+    end 
+
+  # Provides functionality for managing contacts in the csv file.
+  class << self
+
+      def all
+        empty_arr =[]
+         CSV.foreach("contact.csv") do |row|
+          empty_arr.push(Contact.new(row[0], row[1], row[2]))
+         end
+         return empty_arr
+      end
+
+
+
+    def next_id
+      contacts_arr =[]
+      CSV.foreach("contact.csv") do |row|
+          contacts_arr.push(Contact.new(row[0], row[1], row[2]))
+         end
+         contacts_arr.last.id.to_i + 1
+    end
+   
+    def create(name, email)
+        id = next_id
+        new_contact = [name, email, id]
+        CSV.open('contact.csv', "a") do |csv|
+        csv << new_contact
+        end
+        Contact.new(name, email, id)
+     end
+    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
+    def find(id)
+      contacts_arr =[]
+       CSV.foreach("contact.csv") do |row|
+          contacts_arr.push(Contact.new(row[0], row[1], row[2]))
+         end 
+         contacts_arr.detect { |contact| contact.id.to_i == id.to_i }
+    end
+    
+   
+    def search(term)
+      contacts_arr = []
+      CSV.foreach("contact.csv") do |row|
+          contacts_arr.push(Contact.new(row[0], row[1], row[2])) if row[0].include?(term) || row[1].include?(term)
+         end 
+        
+    end
+
+
+      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term
+  end
+  
+end
---
+++ contact_list.rb
@@ -0,0 +1,65 @@
+require_relative 'contact'
+
+class ContactList
+  
+  def self.main_menu
+    if ARGV.empty?
+
+        puts "Here is the list of commands:"
+        puts "      new  - Create a new contact"
+        puts "      list - List all contacts"
+        puts "      show - Show a contact"
+        puts "      search - Search contacts"
+    end
+  end
+
+  def self.run_comm
+
+    #ARGV[0] = gets.chomp.downcase
+      case ARGV[0]
+        when "new"
+         puts "Please enter First Name and Last Name:"
+         name = $stdin.gets.chomp
+         puts "Please enter your Email:"
+         email = $stdin.gets.chomp
+         Contact.create(name, email)
+        puts "#{name} has been added with email address: #{email}."
+          # else    
+          #   puts "That contact already exists."
+          # end
+    
+        when "list"
+          Contact.all.each{|x| puts x.name + x.email}
+            # CSV.open('contact.csv', 'w') do |csv_object|
+            #   contact.array.each do |row_array|
+            #   csv_object << row_array
+            #   end
+            # end
+             # CSV.parse(File.read('contact.csv')) do |row|
+             # puts row.inspect
+        when "show"
+              id = ARGV[1]
+              contact = Contact.find(id)
+              puts "Your contact, #{contact.name} has id no.: #{contact.id}."
+
+        when "search"
+              term = ARGV[1]
+              puts Contact.search(term)
+              #puts Contact.search.each{|x| puts x.name + x.email}
+             #  else
+             #  puts "No contact with : #{term}"
+             # end
+             # contact =[]
+             # CSV.foreach(contact.cvs) do |row|
+              
+          end
+      end        
+  
+end
+  # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
+
+  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+
+
+ContactList.main_menu
+ContactList.run_comm
